99ac902316231007d5a9071df729f46954e21f7d
mw18 <67554339+mw18@users.noreply.github.com>
2021-03-14 21:58:44 -0500

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return <>
-    <Footer fixed="bottom" style={{display: "flex", justifyContent: "center"}}>
-        <small className="text-muted">Last updated 3 mins ago</small>
- </Footer>
-    </>
-}
